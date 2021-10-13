1cb9f51f1584235410c86cfabec1272926e313cd
lucacp <lucas_parnoff@hotmail.com>
2021-10-12 22:49:54 -0300

    iniciando form no javascript

--- scripts.js
+++ scripts.js
@@ -19,40 +19,58 @@ const Modal = {
   }
 }
 
-const transactions = [
-  {
-    id:1,
-    description:"Luz",
-    amount: -50000,
-    date: '23/01/2021'
-  },
-  {
-    id:2,
-    description:"Website",
-    amount: 500000,
-    date: '23/01/2021'
-  },
-  {
-    id:3,
-    description:"Internet",
-    amount: -20000,
-    date: '23/01/2021'
-  },
-  {
-    id:4,
-    description:"App",
-    amount: 200000,
-    date: '23/01/2021'
-  },
-]
-
 const Transaction = {
+  all: [
+    {
+      description:"Luz",
+      amount: -50000,
+      date: '23/01/2021'
+    },
+    {
+      description:"Website",
+      amount: 500000,
+      date: '23/01/2021'
+    },
+    {
+      description:"Internet",
+      amount: -20000,
+      date: '23/01/2021'
+    },
+    {
+      description:"App",
+      amount: 200000,
+      date: '23/01/2021'
+    },
+  ],
+  
+  add(transaction){
+    Transaction.all.push(transaction)
+    App.reload()
+  },
+
+  remove(index){
+    Transaction.all.splice(index,1)
+    App.reload()
+  },
+  
   income(){
-
-  },expenses(){
-
-  },total(){
-
+    let income = 0
+    transactions.forEach((transaction)=>{
+      income+= transaction.amount > 0 ? transaction.amount : 0
+    })
+    return income
+  },
+  
+  expenses(){
+    let expenses = 0
+    transactions.forEach((transaction)=>{
+      expenses+= transaction.amount < 0 ? transaction.amount : 0
+    })
+    return expenses
+  },
+  
+  total(){
+    return Transaction.income() + Transaction.expenses()
   }
 }
 
@@ -60,10 +78,10 @@ const DOM = {
   transactionContainer: document.querySelector('#data-table tbody'),
   addTransaction(transaction,index){
     const tr = document.createElement("tr")
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction,index)
     DOM.transactionContainer.appendChild(tr)
   },
-  innerHTMLTransaction(transaction){
+  innerHTMLTransaction(transaction,index){
     const CSSClass = transaction.amount < 0 ? "expense":"income"
     const amount = Utils.formatCurrency(transaction.amount)
     const html = `
@@ -71,12 +89,25 @@ const DOM = {
                 <td class="${CSSClass}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
-                  <img src="assets/minus.svg" alt="Imagem Remover" />
+                  <img onclick="Transaction.remove(${index})" src="assets/minus.svg" alt="Imagem Remover" />
                 </td>
                 `
     return html
+  },
+  updateBalance(){
+    document
+      .getElementById('incomesDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.income())
+    document
+      .getElementById('expensesDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
+  },
+  clearTransactions(){
+    DOM.transactionContainer.innerHTML = ""
   }
-
 }
 const Utils = {
   formatCurrency(value){
@@ -90,7 +121,28 @@ const Utils = {
     return signal + value
   }  
 }
-transactions.forEach(function(transaction){
-  DOM.addTransaction(transaction)
-})
 
+const Form = {
+  submit(event){
+    event.preventDefault()
+
+    
+  }
+}
+
+const App = {
+  init(){
+    
+    Transaction.all.forEach(function(transaction){
+      DOM.addTransaction(transaction)
+    })
+    
+    DOM.updateBalance()
+  },
+  reload(){
+    DOM.clearTransactions()
+    App.init()
+  }
+}
+
+App.init()
